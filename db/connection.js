2623f1cb8141f3bbc9c77ca04a759ef5438e585d
cemalokten <cemalokten@gmail.com>
2021-11-02 11:38:39 +0000

    Postgres install + connection file
    Co-authored-by: Adriana <82960475+aaadriana@users.noreply.github.com>
    Co-authored-by: Sasha <59370143+cerealenjoyer@users.noreply.github.com>
    Co-authored-by: Barbara <77367593+0bubbles0@users.noreply.github.com>
    Co-authored-by: cemalokten <60609268+cemalokten@users.noreply.github.com>

---
+++ db/connection.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// import postgres-node
+const pg = require('pg');
+
+// connect to Database using url from .env file
+// see (node-postgres.com/features/connecting) - Connection URI
+const DB_URL = process.env.DATABASE_URL;
+
+if (!DB_URL) {
+  throw new Error('Please set the DATABASE_URL environment variable');
+}
+
+const options = {
+  connectionString: DB_URL,
+};
+
+const db = new pg.Pool(options);
+
+export default db;
