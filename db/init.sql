ffdfeff7d8ae212bf9d8361353f21704c7bcafa7
cemalokten <cemalokten@gmail.com>
2021-11-02 16:08:01 +0000

    Database init.sql
    - Populate DB with tables and some content
    - Added image of cushion
    
    Co-authored-by: Adriana <82960475+aaadriana@users.noreply.github.com>
    Co-authored-by: Sasha <59370143+cerealenjoyer@users.noreply.github.com>
    Co-authored-by: Barbara <77367593+0bubbles0@users.noreply.github.com>
    Co-authored-by: cemalokten <60609268+cemalokten@users.noreply.github.com>

---
+++ db/init.sql
@@ -0,0 +1,55 @@
+BEGIN;
+
+DROP TABLE IF EXISTS users, sessions, products, orders, categories CASCADE;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE sessions (
+   sid TEXT PRIMARY KEY,
+   data JSON NOT NULL
+);
+
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    category_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    product_title VARCHAR(100) NOT NULL,
+    product_image TEXT NOT NULL,
+    product_description TEXT NOT NULL,
+    product_price DECIMAL(10, 2) NOT NULL,
+    product_size VARCHAR(100) NOT NULL,
+    product_colour VARCHAR(100) NOT NULL,
+    category_id INTEGER REFERENCES categories(id)
+);
+
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id),
+    order_object JSON NOT NULL,
+    ordered_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+
+INSERT INTO users (email, name, password) VALUES
+(
+  'cushion@gmail.com',
+  'Test Testington',
+  '$2a$10$vwhmLeePYHh6ayoIt42wKuelQ/JaQClYWYsFjr0V25qLLBowiZ.x.'
+);
+
+INSERT INTO categories (category_name) VALUES
+('Cushion'), ('Pillow'), ('Jam'), ('Fidget Spinner');
+
+INSERT INTO products (product_title, product_image, product_description, product_price, product_size, product_colour, category_id ) VALUES
+('Comfy Field Cushion', './image/cushion.png', 'This cushion is amazing, blablabla, can wash it at 40°C. Wonderful design, totally some real people’s photos', 140.99, 'Normal', 'blue', 1 );
+
+
+COMMIT;
